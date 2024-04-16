9904cb23558a4f9664eb82018826e8c50b0f9fa2
sherbieny <sherbieny@outlook.com>
2024-04-16 16:19:24 +0900

    Add migration for pgroonga extension

---
+++ back/db/migrate/20240416061657_add_pgroonga_extension.rb
@@ -0,0 +1,9 @@
+class AddPgroongaExtension < ActiveRecord::Migration[6.0]
+    def up
+        execute "CREATE EXTENSION IF NOT EXISTS pgroonga"
+    end
+
+    def down
+        execute "DROP EXTENSION IF EXISTS pgroonga"
+    end
+end
