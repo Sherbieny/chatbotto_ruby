c94e8ef3f3d913220bb70ae0322bcf45b4fe8e24
sherbieny <sherbieny@outlook.com>
2024-04-16 16:19:33 +0900

    Add generic stuff

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,8 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_16_060431) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_16_061657) do
+  create_schema "pgroonga"
+
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgroonga"
   enable_extension "plpgsql"
 
   create_table "qas", force: :cascade do |t|
